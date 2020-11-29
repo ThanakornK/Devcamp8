50f562ac4a456e3b13467c7a3326f54309b9d527
Sealver <43003841+ThanakornK@users.noreply.github.com>
2020-11-29 18:00:31 +0700

    change enroll data

--- src/component/EnrollData.js
+++ src/component/EnrollData.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const EnrollData = [
     {
-        title:"Even a single mom monkey can paint",
+        title:"สอนทำสีที่แม้แต่ลิงอุรังอุตังแม่เลี้ยงเดี่ยวยังทำตามได้",
         type:"Painting",
         cost:1500.00,
         toytype:"Figure",
         owner:"pondInw00725420924956120"
     },
     {
-        title:"Even a baby seal can do it",
+        title:"สอนปั้นโมเดลที่แม้แต่ลูกแมวน้ำยังทำตามได้",
         type:"Modeling",
         cost:3500.00,
         toytype:"Figure",
