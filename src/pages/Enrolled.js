4139a0e704aedf315499873f72e7ca7cb58f6503
Sealver <43003841+ThanakornK@users.noreply.github.com>
2020-11-29 17:50:26 +0700

    add css of enrolldata

--- src/pages/Enrolled.js
+++ src/pages/Enrolled.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import { EnrollData } from './../component/EnrollData';
+import './../styles/CourseList.css';
 
 function Enrolled() {
     return (
         <div style={{ padding: "10px" }}>
-            <ListGroup>
+            <ListGroup className="CourseList">
                 {EnrollData.map((val, key) => {
                     return (
-                        <ListGroupItem >
+                        <ListGroupItem className="list" onClick={() => {window.location.pathname = val.title}}>
                             <div>{val.title}</div>
                             <div>by {val.owner}</div>
                         </ListGroupItem>
