d8418f27fd9a60248f48a7e31fb98f641f858571
Sealver <43003841+ThanakornK@users.noreply.github.com>
2020-11-30 20:44:03 +0700

    fix height of own side bar

--- src/pages/Owned.js
+++ src/pages/Owned.js
@@ -3,38 +3,12 @@ import { ListGroup, ListGroupItem, Container, Button, Row, Col } from 'react-boo
 import { OwnData } from './../component/OwnData';
 import OwnSideBar from './../component/OwnSideBar';
 import ContentOwnField from './../component/ContentOwnField';
+import { Height } from '@material-ui/icons';
 
 export default class Owned extends React.Component {
     render() {
         return (
-            // <div style={{ padding: "10px" }}>
-            //     <Container fluid="true">
-            //         <Row >
-            //             <Col sm>
-            //                 <ListGroup className="CourseList">
-            //                     {OwnData.map((val, key) => {
-            //                         return (
-            //                             <ListGroupItem className="list">
-            //                                 <div><h4>{val.title}</h4></div>
-            //                                 <Container className="DetailCourse" fluid="true">
-            //                                     <div style={{ paddingRight: "10px" }}>Type: {val.type} </div>
-            //                                     <div style={{ paddingRight: "20px" }}>Toy type: {val.toytype}</div>
-            //                                     <div style={{ paddingRight: "5px" }}>by {val.owner}</div>
-            //                                 </Container>
-            //                                 <div style={{ fontSize: "18px" }}>price: {val.cost} baht</div>
-            //                                 <Button>Edit</Button>
-            //                             </ListGroupItem>
-            //                         )
-            //                     })}
-            //                 </ListGroup>
-            //             </Col>
-            //             <Col sm={2}>
-            //                 <OwnSideBar />
-            //             </Col>
-            //         </Row>
-            //     </Container>
-            // </div>
-            <div style={{ display: "flex", flexDirection: "row" }} fluid="true">
+            <div style={{ display: "flex", flexDirection: "row", height: "95vh" }} fluid="true">
                 <ContentOwnField />
                 <div>
                     <OwnSideBar />
