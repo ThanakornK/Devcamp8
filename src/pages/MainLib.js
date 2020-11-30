00d609668923e207c761acbda3792308877c0b63
Sealver <43003841+ThanakornK@users.noreply.github.com>
2020-11-30 11:31:46 +0700

    show content of enrolled

--- src/pages/MainLib.js
+++ src/pages/MainLib.js
@@ -7,20 +7,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LibNav from './../component/LibNav';
 import Enrolled from './Enrolled';
 import Owned from './Owned';
+import ContentCourse from './../pages/ContentCourse';
 
 export default class MainLib extends React.Component {
-    render () {
+    render() {
         return (
             <div className="mainLib" >
                 <LibNav />
-                {/* <BrowserRouter>
-                    <Switch>
-                        <Route path='/library' component={Enrolled} />
-                        <Route path='/owned' component={Owned} />
-                    </Switch>
-                </BrowserRouter> */}
+                <BrowserRouter>
+                    <Route path="/1" component={ContentCourse}></Route>
+                </BrowserRouter>
             </div>
-    
+
         );
     }
 }
