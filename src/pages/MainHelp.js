4167866c78d6b545c11facfa357b591500fe126b
Napat <napat.amorn@ku.th>
2020-12-01 11:46:16 +0700

    link Contact Us Page

--- src/pages/MainHelp.js
+++ src/pages/MainHelp.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './../styles/MainHelp.css';
 import { Row, Col } from 'react-bootstrap';
 import { render } from '@testing-library/react';
+import Contact from './../pages/Contact.js';
 
 export default class MainHelp extends React.Component {
     render () {
         return (
             <div>
-                <Row style={{border:"1px black"}}>
-                    <Col> test1
-                    </Col>
-                </Row>
+                <Contact/>
             </div>
+
         )
     }
 }
