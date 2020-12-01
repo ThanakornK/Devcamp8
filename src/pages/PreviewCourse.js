cfc188ce6734c78b5953beb7005ea9d17d9dedbf
Sealver <43003841+ThanakornK@users.noreply.github.com>
2020-12-01 16:22:02 +0700

    add preview course in store

---
+++ src/pages/PreviewCourse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PreviewForm from './../component/PreviewForm';
+
+function PreviewCourse() {
+    return (
+        <div style={{height:"115vh"}}>
+            <PreviewForm/>
+        </div>
+    )
+}
+
+export default PreviewCourse
