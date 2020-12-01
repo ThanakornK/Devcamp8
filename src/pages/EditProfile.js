7e86195089005eb7e1480485ebfd919a74d351dc
Napat <napat.amorn@ku.th>
2020-12-01 15:09:58 +0700

    Refactor Edit Page

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { InputGroup, FormControl, Form } from 'react-bootstrap';
 import './../styles/EditProfile.css';
 
 export default class EditProfile extends Component {
@@ -9,108 +10,19 @@ export default class EditProfile extends Component {
                 <div className="profileImgHolder">
                     <img src="./images/guest_profile.png" className="profileImg"/>
                 </div>
-                <form className="text-center">
-                    <div class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <br/>
-                        <input type="file" class="form-control-file" id="profileImg" className="uploadProfileCenter" />
-                    </div>
-                </form>
-                <div className="userName">Username: PondTHElnwZAAA</div>
-                
-                <form className="text-center">
-                <div class="form-group">
-                    <textarea class="form-control" id="userBio" rows="8">
-                    ชายหนุ่มผู้มีความมุ่งมั่นที่จะเป็นเจ้าแห่งการต่อโมเดลและการลงสี เขาเริ่มทำการเก็บรวบรวมโมเดลจนครบ 7 ตัวเพื่อขอพรกับเทพเจ้าโมเดลให้ชุบชีวิตแฟนสาว
-                    </textarea>
-                </div>
-                </form>
-                
-                <div className="collectionHeader">My Collection</div>
-           
-  <div class="card-group">
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-    <div class="card" className="col-sm-4">
-      <img className="collectionImg" src="./images/guest_profile.png" alt="Card image cap"/>
-      <form className="center">
-                    <span class="form-group">
-                        <label for="profileImg">Upload your Profile Picture</label>
-                        <input type="file" class="form-control-file" id="profileImg"/>
-                    </span>
-                </form>
-    </div>
-  </div>
+
+                    <Form>
+                        <Form.Group style={{marginLeft:520}}> 
+                            <Form.Label>Upload Image</Form.Label>
+                            <Form.File id="newProfileImg"/>
+                        </Form.Group>
+
+                        <Form.Label className="bioHead">Username: PondTHElnwZAAA</Form.Label>
+
+                        <InputGroup className="FormInput">
+                            <FormControl as="textarea" placeholder="Enter Your Description" col="50" style={{marginLeft:0}} />
+                        </InputGroup>
+                    </Form>
 
   <a class="btn btn-primary btn-lg float-right" href="/profile" role="button">Confirm</a>
             </div>
