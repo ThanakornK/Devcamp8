d11d4ebaf1b1008cd1f18b5c82cd710aee1628e4
Thanakit Yannam <thanakit.ya@ku.th>
2020-12-02 15:10:25 +0700

    add login navbar

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -4,23 +4,55 @@ import { Button, Container, Row, Col, Navbar, Nav, NavDropdown } from 'react-boo
 import Logo from './../images/logo.png';
 
 export default class Menu extends React.Component {
-    render () {
+
+  render() {
+    if(localStorage.getItem('isLogin') == 1){
       return (
-        <Navbar style={{backgroundColor:"#FFD800"}} expand="lg">
+        <Navbar style={{ backgroundColor: "#FFD800" }} expand="lg">
           <Navbar.Brand href="/library">
             <img alt="" src={Logo} width="150" height="50" className="d-inline align-top" />
           </Navbar.Brand>
           <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
             <Nav >
               <NavDropdown title="Username">
-              <NavDropdown.Item href="/profile/1">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="/profile/1">Log out</NavDropdown.Item>
+                <NavDropdown.Item href={"/profile/" + localStorage.getItem('user_id')}>Profile</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => {
+                  localStorage.setItem('user_id', 0);
+                  localStorage.setItem('isLogin', 0)
+                  window.location.pathname = '/login'
+                }}>Log out</NavDropdown.Item>
               </NavDropdown>
-              
-              <img src="/images/guest_profile.png" style={{width:"40px", height:"40px"}} />
+
+              <img src="/images/guest_profile.png" style={{ width: "40px", height: "40px" }} />
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-      )
+    )
     }
+    return (
+      <Navbar style={{ backgroundColor: "#FFD800" }} expand="lg">
+          <Navbar.Brand href="/library">
+            <img alt="" src={Logo} width="150" height="50" className="d-inline align-top" />
+          </Navbar.Brand>
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            {/* <Nav >
+              <NavDropdown title="Username">
+                <NavDropdown.Item href={"/profile/" + localStorage.getItem('user_id')}>Profile</NavDropdown.Item>
+                <NavDropdown.Item onClick={() => {
+                  localStorage.setItem('user_id', 0);
+                  localStorage.setItem('isLogin', 0)
+                  window.location.pathname = '/login'
+                }}>Log out</NavDropdown.Item>
+              </NavDropdown>
+
+              <img src="/images/guest_profile.png" style={{ width: "40px", height: "40px" }} />
+            </Nav> */}
+          </Navbar.Collapse>
+        </Navbar>
+    )
+    
+      
+  }
+
+  
 }
