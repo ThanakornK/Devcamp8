e4431257202a4b8b150b6bdae1b40ce1b9f09516
Thanakit Yannam <thanakit.ya@ku.th>
2020-12-02 22:56:43 +0700

    fix register bug

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -2,8 +2,29 @@ import React from 'react';
 import './../App.css';
 import { Button, Container, Row, Col, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Logo from './../images/logo.png';
+import axios from 'axios'
+import url from '../service/apiService'
 
 export default class Menu extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: []
+    };
+  }
+
+  componentDidMount() {
+    // console.log(axios.get(`http://8cf0d250a525.ngrok.io/api/courses/1`).data)
+    // console.log(userData)
+    axios.get(url+`/api/profile/`+localStorage.getItem('user_id'))
+        .then(res => {
+            const user = res.data;
+            console.log(user) ;
+            this.setState({user: user});
+        })
+        console.log(this.state.user);
+    }
 
   render() {
     if(localStorage.getItem('isLogin') == 1){
@@ -14,16 +35,17 @@ export default class Menu extends React.Component {
           </Navbar.Brand>
           <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
             <Nav >
-              <NavDropdown title="Username">
+              <NavDropdown title={localStorage.getItem('username')}>
                 <NavDropdown.Item href={"/profile/" + localStorage.getItem('user_id')}>Profile</NavDropdown.Item>
                 <NavDropdown.Item onClick={() => {
                   localStorage.setItem('user_id', 0);
                   localStorage.setItem('isLogin', 0)
+                  localStorage.setItem('username', ' ')
                   window.location.pathname = '/login'
                 }}>Log out</NavDropdown.Item>
               </NavDropdown>
 
-              <img src="/images/guest_profile.png" style={{ width: "40px", height: "40px" }} />
+              <img src={url+this.state.user.img_pro} style={{ width: "40px", height: "40px" }} />
             </Nav>
           </Navbar.Collapse>
         </Navbar>
--- src/component/RegisterForm.js
+++ src/component/RegisterForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './../styles/Login.css';
 // import {authen} from './../App.js';
 import axios from 'axios'
@@ -6,6 +6,22 @@ import url from '../service/apiService'
 
 
 function RegisterForm() {
+
+    const [state, setState] = useState({
+        course: [],
+        email: '',
+        pass: '',
+        name:''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        console.log(state.course.id)
+        setState({
+            ...state,
+            [name]: value
+        })
+    }
     return (
         <div>
             <form className="form">
@@ -13,15 +29,18 @@ function RegisterForm() {
                 <br />
                 <div class="form-group">
                     <label for="inputEmail">Email</label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email"
+                        name='email' id="email" value={state.email} onChange={handleChange} />
                 </div>
                 <div class="form-group">
                     <label for="inputUserName">UserName</label>
-                    <input type="userName" class="form-control" id="name" placeholder="Enter User Name" />
+                    <input type="userName" class="form-control" id="name" placeholder="Enter User Name"
+                        name='name' id="name" value={state.name} onChange={handleChange} />
                 </div>
                 <div class="form-group">
                     <label for="inputPassword">Password</label>
-                    <input type="password" class="form-control" id="pass1" placeholder="Password" />
+                    <input type="password" class="form-control" id="pass" placeholder="Password"
+                        name='pass' id="pass" value={state.pass} onChange={handleChange} />
                 </div>
                 {/* <div class="form-group">
                         <label for="inputPassword2">Confirm Password</label>
@@ -29,30 +48,31 @@ function RegisterForm() {
                     </div> */}
             </form>
             <div className="loginButton">
-            <button class="btn btn-primary" onClick={() => {
-                var formData = new FormData();
-                formData.append("email", document.getElementById('email').value)
-                formData.append("pass", document.getElementById('pass1').value)
-                formData.append("name", document.getElementById('name').value)
-                axios.post(url + '/api/register', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                }).then(res => {
-                    console.log(res.data)
-                    if (res.data == 'fail') {
-                        // send error message
-                    }
-                    else {
-                        // localStorage.setItem('isLogin', 1);
-                        // localStorage.setItem('user_id', res.data.id);
-                        window.location.pathname = '/login';
-                    }
+                <button class="btn btn-primary" onClick={() => {
+                    var formData = new FormData();
+                    formData.append("email", state.email)
+                    formData.append("pass", state.pass)
+                    formData.append("name", state.name)
+                    // console.log(formData.data.email ;
+                    axios.post(url + '/api/register', formData, {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    }).then(res => {
+                        console.log(res.data)
+                        if (res.data == 'fail') {
+                            // send error message
+                        }
+                        else {
+                            // localStorage.setItem('isLogin', 1);
+                            // localStorage.setItem('user_id', res.data.id);
+                            window.location.pathname = '/login';
+                        }
 
-                })
-            }}>Register</button>
+                    })
+                }}>Register</button>
             </div>
-            
+
         </div>
     )
 }
